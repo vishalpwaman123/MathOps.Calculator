8fd4d00a648b9555de030098529eb36790e48e73
vishalpwaman123 <vishalpwaman123@gmail.com>
2026-07-04 19:49:50 +0530

    Add Substract operation for int and double
    Extend MathCalculator with a subtraction method overloaded for
    int and double, mirroring the existing Add overloads.

--- Vishal.MathOperations/Calculator.cs
+++ Vishal.MathOperations/Calculator.cs
@@ -5,6 +5,10 @@
         public int Add(int a, int b) => a + b;
 
         public double Add(double a, double b) => a + b;
+
+        public int Substract(int a, int b) => a - b;
+
+        public double Substract(double a, double b) => a - b;
         
     }
 }
